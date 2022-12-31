e7892a89198545d9b07803d31bd1806769fb3818
Astra <astra@astrra.space>
2022-12-31 12:14:29 +0200

    Bugfixes and a live status page

--- gemserv.ino
+++ gemserv.ino
@@ -10,7 +10,7 @@
 
 #include "config.h"
 
-#define	CHUNK_SIZE 256
+#define  CHUNK_SIZE 256
 
 // response headers
 static const char *HEADER_GEM_OK            = "20 text/gemini\r\n";                 // .gmi
@@ -29,6 +29,13 @@ void setup()
     // setup LittleFS
     LittleFSConfig cfg;
     cfg.setAutoFormat(false);
+    if(!LittleFS.begin()){
+      Serial.println("Error mounting LittleFS!");
+      return;
+    }
+
+    // setup serial
+    Serial.begin(74880);
 
     // connect to wifi as defined in config.h
     WiFi.mode(WIFI_STA);
@@ -37,12 +44,20 @@ void setup()
 
     // wait for connection
     while(WiFi.status() != WL_CONNECTED){delay(1000);}
+    Serial.println("Connected to WiFi");
+    Serial.println("IP address: " + WiFi.localIP().toString());
 
     // add private cert and key
     BearSSL::X509List *serverCertList = new BearSSL::X509List(server_cert);
     BearSSL::PrivateKey *serverPrivateKey = new BearSSL::PrivateKey(server_private_key);
     
     server.setRSACert(serverCertList, serverPrivateKey);
+
+    // list files in LittleFS
+    Dir dir = LittleFS.openDir("/gemini/");
+    while (dir.next()){
+        Serial.println(dir.fileName());
+    }
 
     // begin accepting connections
     server.begin();
@@ -61,7 +76,31 @@ void loop()
 
             if (!LittleFS.exists(request.getFile_path())) // if file does not exist
             { 
+                if(request.getFile_path().equals("/gemini/status.gmi")) // if request is for status page then generate it on the fly
+                {
+                    Serial.println("Sending status page");
+                    incomingConnection.read();
+                    incomingConnection.write((uint8_t*)HEADER_GEM_OK, strlen(HEADER_GEM_OK));
+                    incomingConnection.write("# Live status\n");
+                    incomingConnection.write(("This page was generated on the fly at " + String(millis()/1000) + " seconds after boot.\n").c_str());
+                    incomingConnection.write(("IP address: " + WiFi.localIP().toString() + "\n").c_str());
+                    incomingConnection.write(("Free memory: " + String(ESP.getFreeHeap()) + " bytes\n").c_str());
+                    incomingConnection.write(("Power voltage: " + String(analogRead(0)) + "V\n").c_str());
+                    incomingConnection.write(("Uptime: " + String(millis()/1000) + " seconds\n").c_str());
+                    incomingConnection.write(("Connected to: " + String(SSID) + "\n").c_str());
+                    incomingConnection.write(("Hostname: " + String(HOSTNAME) + "\n").c_str());
+                    incomingConnection.write(("Signal strength: " + String(WiFi.RSSI()) + "dBm\n").c_str());
+                    incomingConnection.write("### Files:\n");
+                    Dir direct = LittleFS.openDir("/gemini/");
+                    while (direct.next()){
+                        incomingConnection.write(("=> " + direct.fileName() + " " + direct.fileName()).c_str());
+                        incomingConnection.write("\n");
+                    }
+                    incomingConnection.flush();
+                    incomingConnection.stop();
+                }
                 // Send file not found header
+                Serial.println("File not found: " + request.getFile_path());
                 incomingConnection.read();
                 incomingConnection.write((uint8_t*)HEADER_NOT_FOUND, strlen(HEADER_NOT_FOUND));
                 incomingConnection.flush();
@@ -74,6 +113,7 @@ void loop()
                 if (!file.isFile()) // error opening file
                 {
                     file.close();
+                    Serial.println("Error opening file: " + request.getFile_path());
 
                     incomingConnection.read();
                     incomingConnection.write((uint8_t*)HEADER_INTERNAL_FAIL, strlen(HEADER_INTERNAL_FAIL));
@@ -82,6 +122,7 @@ void loop()
                 }
                 else // file is OK
                 {
+                    Serial.println("Sending file: " + request.getFile_path());
                     incomingConnection.read();  // read the rest of the incomig data
                                                 
                     String extension = request.getFile_ext(); // get the file extension and send the required header
